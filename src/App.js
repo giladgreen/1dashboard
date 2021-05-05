c7bd8ac3622c4b5a45d12e158cdfa307923e1a46
gilad green <gilad.green@autodesk.com>
2021-05-05 18:16:13 +0300

    a

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="welcome-page">
+
+      <div id="welcome-header">
+          <div>
+              <span className="uppercase">welcome to</span>
+
+          </div>
+          <div >
+              <span className="app-name">1Dashboard</span>
+
+          </div>
+
+      </div>
+        <div >
+            <img id="pie-image" src="pie.png" />
+        </div>
+    </div>
+  );
+}
+
+export default App;
