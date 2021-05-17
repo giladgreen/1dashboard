30e6b888610039fcf75c3a324426def0c7a055af
gilad green <gilad.green@autodesk.com>
2021-05-17 09:29:09 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/app.css';
 
 function App() {
+
   return (
     <div id="welcome-page">
 
       <div id="welcome-header">
-          <div>
-              <span className="uppercase">welcome to</span>
 
-          </div>
           <div >
               <span className="app-name">1Dashboard</span>
 
@@ -17,7 +14,12 @@ function App() {
 
       </div>
         <div >
-            <img id="pie-image" src="pie.png" />
+
+            <img id="pie-image-full"  className="pie-image" src="https://giladgreen.github.io/1dashboard/pie-full.png" />
+            <img id="pie-image-1"  className="pie-image" src="https://giladgreen.github.io/1dashboard/pie_1.png" />
+            <img id="pie-image-2"  className="pie-image" src="https://giladgreen.github.io/1dashboard/pie_2.png" />
+            <img id="pie-image-3"  className="pie-image" src="https://giladgreen.github.io/1dashboard/pie_3.png" />
+            <img id="pie-image-4"  className="pie-image" src="https://giladgreen.github.io/1dashboard/pie_4.png" />
         </div>
     </div>
   );
